8a006d546cac5cc278bc5c692200cd8f31af5607
Nr755 <mi4766si-s@student.lu.se>
2025-03-25 15:20:18 +0100

    Done with lab2

---
+++ Labs/Lab2/SudokuTest.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Sudoku -- import your Sudoku module
+
+main :: IO ()
+main = do
+  let goodSudoku = "1234341243212143"
+  let badSudoku = "1134341243212143"
+
+  putStrLn $ "Verifying valid Sudoku:   " ++ show (verifySudoku goodSudoku)
+  putStrLn $ "Verifying invalid Sudoku: " ++ show (verifySudoku badSudoku)
